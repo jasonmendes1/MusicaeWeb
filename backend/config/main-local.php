42dfebcb590e0a10aa39cb556b311718b15981ca
Luckdude <DEFTFHMYRER>
2019-11-27 19:24:41 +0000

    CorrecaoV2

---
+++ backend/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'RZpcRB0w7B3uDL2I8d8mwye_4pV0ZOvy',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
