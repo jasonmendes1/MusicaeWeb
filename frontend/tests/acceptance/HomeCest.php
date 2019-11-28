7b8fe48159775cc64f929d04fa0b2274e05931c9
Pedro Alves <pedromiguel.alves8@gmail.com>
2019-11-28 10:09:28 +0000

    Acceptance Test

--- frontend/tests/acceptance/HomeCest.php
+++ frontend/tests/acceptance/HomeCest.php
@@ -2,19 +2,34 @@
 namespace frontend\tests\acceptance;
 
 use frontend\tests\AcceptanceTester;
-use yii\helpers\Url;
 
 class HomeCest
 {
-    public function checkHome(AcceptanceTester $I)
+    /*public function checkHome(AcceptanceTester $I)
     {
-        $I->amOnPage(Url::toRoute('/site/index'));
-        $I->see('My Application');
+        $I->amOnPage('musicaeweb/frontend/web/index-test.php');
+        $I->see('Musicae');
 
         $I->seeLink('About');
         $I->click('About');
         $I->wait(2); // wait for page to be opened
 
         $I->see('This is the About page.');
+    }*/
+
+    public function checkLogin(AcceptanceTester $I)
+    {
+        $I->amOnPage('musicaeweb/frontend/web/index-test.php');
+        $I->wait(2); // wait for page to be opened
+        $I->seeLink('Login');
+        $I->click('Login');
+        $I->wait(2); // wait for page to be opened
+        $I->see('Username');
+        $I->fillField('LoginForm[username]', 'pedro');
+        $I->wait(1); // wait for page to be opened
+        $I->fillField('LoginForm[password]', '123456');
+        $I->wait(1); // wait for page to be opened
+        $I->click('login-button');
+        $I->wait(5); // wait for page to be opened
     }
 }
