5bb975dbb6f573c8524413a00f27b781ae1b0a32
Jason Mendes <jason_mendes_1998@hotmail.com>
2020-01-09 12:15:55 +0000

    Revert "Api auth Done"
    This reverts commit 2adaed6018807f1936390bcd8358d7783289df8b.

--- backend/modules/v1/controllers/ProfilesController.php
+++ backend/modules/v1/controllers/ProfilesController.php
@@ -3,8 +3,6 @@
 namespace backend\modules\v1\controllers;
 
 use yii\rest\ActiveController;
-use yii\filters\auth\HttpBasicAuth;
-
 
 /**
  * Default controller for the `v1` module
@@ -12,24 +10,4 @@ use yii\filters\auth\HttpBasicAuth;
 class ProfilesController extends ActiveController
 {
     public $modelClass = 'common\models\Profiles';
-
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        $behaviors['authenticator'] = [
-        'class' => HttpBasicAuth::className(),
-        'auth' => [$this, 'auth']
-        ];
-        return $behaviors;
-    }
-
-    public function auth($username, $password)
-    { 
-        $user = \common\models\
-        User::findByUsername($username);
-        if ($user && $user->validatePassword($password)) {
-        return $user;
-        }
-        return null;
-    }
 }
