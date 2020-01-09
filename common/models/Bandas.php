3c0f7ae26eb765922a62d31c8a10133d10fb8da3
Jason Mendes <jason_mendes_1998@hotmail.com>
2020-01-09 16:00:41 +0000

    MESSAGING

--- common/models/Bandas.php
+++ common/models/Bandas.php
@@ -174,4 +174,51 @@ class Bandas extends \yii\db\ActiveRecord
     {
         return $this->hasMany(Musicos::className(), ['Id' => 'IdMusico'])->viaTable('listabandas', ['IdBanda' => 'Id']);
     }
+
+    public function afterSave($insert, $changedAttributes)
+    {
+        parent::afterSave($insert, $changedAttributes);
+
+        //Obter dados do registo em causa
+        $id=$this->id;
+        $designacao=$this->designacao;
+        $preco=$this->preco;
+        $img=$this->img; 
+        $myObj=new \stdClass();
+        $myObj->id=$id;
+        $myObj->designacao=$designacao;
+        $myObj->preco=$preco;
+        $myObj->img=$img;
+        $myJSON = json_encode($myObj);
+        if($insert)
+            $this->FazPublish("INSERT",$myJSON);
+        else
+            $this->FazPublish("UPDATE",$myJSON);
+    } 
+
+    public function afterDelete()
+    {
+        parent::afterDelete();
+        $prod_id= $this->id;
+        $myObj=new \stdClass();
+        $myObj->id=$prod_id;
+        $myJSON = json_encode($myObj);
+        $this->FazPublish("DELETE",$myJSON);
+    }
+
+    public function FazPublish($canal,$msg)
+    {
+        $server = "127.0.0.1";
+        $port = 1883;
+        $username = "jason"; // set your username
+        $password = "123456"; // set your password
+        $client_id = "phpMQTT-publisher"; // unique!
+        $mqtt = new \app\mosquitto\phpMQTT($server, $port, $client_id);
+        if ($mqtt->connect(true, NULL, $username, $password)){
+            $mqtt->publish($canal, $msg, 0);
+            $mqtt->close();
+        }
+        else { file_put_contents("debug.output","Time out!"); }
+    }
+ 
 }
