05b991ba525934a875b7b7a2ab2f823007c1d423
Jason Mendes <jason_mendes_1998@hotmail.com>
2020-01-09 12:16:24 +0000

    Api basic Auth Done

--- backend/modules/v1/controllers/ProfilesController.php
+++ backend/modules/v1/controllers/ProfilesController.php
@@ -3,6 +3,8 @@
 namespace backend\modules\v1\controllers;
 
 use yii\rest\ActiveController;
+use yii\filters\auth\HttpBasicAuth;
+
 
 /**
  * Default controller for the `v1` module
@@ -10,4 +12,24 @@ use yii\rest\ActiveController;
 class ProfilesController extends ActiveController
 {
     public $modelClass = 'common\models\Profiles';
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+        'class' => HttpBasicAuth::className(),
+        'auth' => [$this, 'auth']
+        ];
+        return $behaviors;
+    }
+
+    public function auth($username, $password)
+    { 
+        $user = \common\models\
+        User::findByUsername($username);
+        if ($user && $user->validatePassword($password)) {
+        return $user;
+        }
+        return null;
+    }
 }
