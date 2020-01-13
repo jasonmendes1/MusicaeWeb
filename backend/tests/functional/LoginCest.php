3010236c32731a25c640252c665c9c66602778a3
Pedro Alves <pedromiguel.alves8@gmail.com>
2020-01-13 22:36:01 +0000

    test

--- backend/tests/functional/LoginCest.php
+++ backend/tests/functional/LoginCest.php
@@ -10,26 +10,6 @@ use common\fixtures\UserFixture;
  */
 class LoginCest
 {
-    /**
-     * Load fixtures before db transaction begin
-     * Called in _before()
-     * @see \Codeception\Module\Yii2::_before()
-     * @see \Codeception\Module\Yii2::loadFixtures()
-     * @return array
-     */
-    public function _fixtures()
-    {
-        return [
-            'user' => [
-                'class' => UserFixture::className(),
-                'dataFile' => codecept_data_dir() . 'login_data.php'
-            ]
-        ];
-    }
-    
-    /**
-     * @param FunctionalTester $I
-     */
     public function loginUser(FunctionalTester $I)
     {
         $I->amOnPage(\yii::$app->homeUrl);
