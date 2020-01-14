6612606cd9f32794a659e2379a7149b669c0b4b8
Jason Mendes <jason_mendes_1998@hotmail.com>
2020-01-14 00:49:59 +0000

    teste banda create

--- backend/tests/functional/CreateBandCest.php
+++ backend/tests/functional/CreateBandCest.php
@@ -7,7 +7,8 @@ use common\fixtures\UserFixture;
 
 class CreateBandCest
 {
-    public function tryCreateBand(FunctionalTester $I)
+/*
+    public function tryLogin(FunctionalTester $I)
     {
         $I->amOnPage(\yii::$app->homeUrl);
         $I->click('Login');
@@ -17,16 +18,21 @@ class CreateBandCest
         $I->see('Logout (pedro)', 'form button[type=submit]');
         $I->dontSeeLink('Login');
         $I->dontSeeLink('Signup');
-        
-        $I->amOnPage('/musicaeweb/backend/web/banda/create');
-        //$I->click('create');
-        $I->fillField('Bandas[Nome]', 'BandaTeste');
-        $I->fillField('Bandas[Descricao]', 'BandaTeste');
-        $I->fillField('Bandas[Localizacao]', 'BandaTeste');
-        $I->fillField('Bandas[Contacto]', '123456789');
-        $I->fillField('Bandas[Logo]', '1');
-        $I->fillField('Bandas[Removida]', '1');
+    }
+*/
+    public function tryCreateBand(FunctionalTester $I)
+    {
+        $I->amOnPage('/banda/create');
+        //$I->click('Create');
+        $I->fillField('Nome', 'BandaTeste');
+        $I->fillField('Descricao', 'BandaTeste');
+        $I->fillField('Localizacao', 'BandaTeste');
+        $I->fillField('Contacto', '123456789');
+        $I->fillField('Logo', '1');
+        $I->fillField('Removida', '1');
         $I->fillField('Bandas[IdGenero]', '1');
+        $I->click('Save');
+
 
     }
 }
