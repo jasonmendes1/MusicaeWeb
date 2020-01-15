eb76f47b593425afb3d5eac3528d15e6297b533f
Pedro Alves <pedromiguel.alves8@gmail.com>
2020-01-15 01:15:41 +0000

    Update BandaHistoricoTest.php

--- backend/tests/unit/BandaHistoricoTest.php
+++ backend/tests/unit/BandaHistoricoTest.php
@@ -26,13 +26,35 @@ class BandaHistoricoTest extends \Codeception\Test\Unit
         $this->assertFalse($user->validate(['Duracao']));
     }
 
-    function testSavingGenero()
+    function testSavingBandasHistorico()
     {
         $user = new BandasHistorico();
         $user->IdMusico = 1;
-        $user->Duracao = '2017-07-23';
+        $user->Duracao = '2000-01-01';
         $user->IdBanda = 3;
         $user->save();
-        $this->tester->seeInDatabase('bandasHistorico', ['Duracao' => '2017-07-23']);
+        $this->tester->seeInDatabase('bandasHistorico', ['Duracao' => '2000-01-01']);
+    }
+
+    function testBandasHistoricoCanBeChanged()
+    {
+        $id = $this->tester->grabRecord('common\models\BandasHistorico', ['Duracao' => '2000-01-01']);
+
+        $user = BandasHistorico::findOne($id);
+        $user->Duracao = ('2020-02-02');
+        $user->save();
+
+        $this->tester->seeRecord('common\models\BandasHistorico', ['Duracao' => '2020-02-02']);
+        $this->tester->dontSeeRecord('common\models\BandasHistorico', ['Duracao' => '2000-01-01']);
+    }
+
+    function testBandasHistoricoDeleted()
+    {
+        $id = $this->tester->grabRecord('common\models\BandasHistorico', ['Duracao' => '2020-02-02']);
+
+        $user = BandasHistorico::findOne($id);
+        $user->delete();
+
+        $this->tester->dontSeeRecord('common\models\BandasHistorico', ['Duracao' => '2020-02-02']);
     }
 }
